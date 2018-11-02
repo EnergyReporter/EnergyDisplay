84d7ff27956c27d6351fd0e87af12d2474064f36
RP <rapaat@gmail.com>
2018-11-02 06:55:02 +0200

    Changed RPi zero pins

--- python/epdif.py
+++ python/epdif.py
@@ -29,10 +29,10 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin definition
-RST_PIN         = 17
-DC_PIN          = 25
-CS_PIN          = 8
-BUSY_PIN        = 24
+RST_PIN         = 2
+DC_PIN          = 3
+CS_PIN          = 10
+BUSY_PIN        = 0
 
 # SPI device, bus = 0, device = 0
 SPI = spidev.SpiDev(0, 0)
